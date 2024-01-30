620da5bd3cfe02a342225872befb8eb0b22f7809
Ryan-Robot12 <ratack0@gmail.com>
2024-01-30 14:41:13 -0500

    comments cleanup

--- src/main/java/frc/robot/commands/DriveToPos.java
+++ src/main/java/frc/robot/commands/DriveToPos.java
@@ -39,6 +39,7 @@ public class DriveToPos extends DriveCommandBase {
   private Pose2d endPose;
   private double finalX, finalY, finalRot;
 
+  // TODO: might not work, might just use NewDriveToPos.java instead
   /** Creates a new DriveToAmp. */
   public DriveToPos(DriveSubsystem driveSubsystem, VisionSubsystem visionSubsystem, BooleanSupplier isFinished, double finalX, double finalY, double finalRot) {
     super(driveSubsystem, visionSubsystem);
@@ -53,44 +54,24 @@ public class DriveToPos extends DriveCommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // List<PathPoint> pathPoints = new ArrayList<PathPoint>();
-    // pathPoints.add(new PathPoint(driveSubsystem.getPose().getTranslation()));
+    List<PathPoint> pathPoints = new ArrayList<PathPoint>();
+    pathPoints.add(new PathPoint(driveSubsystem.getPose().getTranslation()));
 
-    // pathPoints.add(new PathPoint(new Translation2d(finalX, finalY), new RotationTarget(0, Rotation2d.fromDegrees(finalRot))));
+    pathPoints.add(new PathPoint(new Translation2d(finalX, finalY), new RotationTarget(0, Rotation2d.fromDegrees(finalRot))));
 
-    // Rotation2d startRotation = driveSubsystem.getRotation2d();
-    // PathPlannerPath path = PathPlannerPath.fromPathPoints(pathPoints, TrajectoryConstants.PATH_CONSTRAINTS, new GoalEndState(0, Rotation2d.fromDegrees(finalRot)));
-    // PathPlannerTrajectory trajectory = new PathPlannerTrajectory(path, new ChassisSpeeds(), startRotation);
+    Rotation2d startRotation = driveSubsystem.getRotation2d();
+    PathPlannerPath path = PathPlannerPath.fromPathPoints(pathPoints, TrajectoryConstants.PATH_CONSTRAINTS, new GoalEndState(0, Rotation2d.fromDegrees(finalRot)));
+    PathPlannerTrajectory trajectory = new PathPlannerTrajectory(path, new ChassisSpeeds(), startRotation);
 
-    // controllerCommand = new RealTimeSwerveControllerCommand(
-    //   trajectory, 
-    //   SmarterDashboardRegistry::getPose, 
-    //   (ChassisSpeeds speeds) -> driveSubsystem.drive(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false), 
-    //   isFinished, 
-    //   endPose, 
-    //   driveSubsystem
-    // );
-    // controllerCommand.schedule();
-// Create a list of bezier points from poses. Each pose represents one waypoint.
-// The rotation component of the pose should be the direction of travel. Do not use holonomic rotation.
-List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(
-        new Pose2d(1.0, 1.0, Rotation2d.fromDegrees(0)),
-        new Pose2d(3.0, 1.0, Rotation2d.fromDegrees(0)),
-        new Pose2d(5.0, 3.0, Rotation2d.fromDegrees(90))
-);
-
-// Create the path using the bezier points created above
-PathPlannerPath path = new PathPlannerPath(
-        bezierPoints,
-        new PathConstraints(3.0, 3.0, 2 * Math.PI, 4 * Math.PI), // The constraints for this path. If using a differential drivetrain, the angular constraints have no effect.
-        new GoalEndState(0.0, Rotation2d.fromDegrees(-90)) // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
-);
-
-// Prevent the path from being flipped if the coordinates are already correct
-path.preventFlipping =true; 
-// Since we are using a holonomic drivetrain, the rotation component of this pose
-// represents the goal holonomic rotation
-
+    controllerCommand = new RealTimeSwerveControllerCommand(
+      trajectory, 
+      SmarterDashboardRegistry::getPose, 
+      (ChassisSpeeds speeds) -> driveSubsystem.drive(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false), 
+      isFinished, 
+      endPose, 
+      driveSubsystem
+    );
+    controllerCommand.schedule();
  }
 
   // Called every time the scheduler runs while the command is scheduled.
