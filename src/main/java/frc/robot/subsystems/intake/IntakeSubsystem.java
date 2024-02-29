e710874a95c836677a9529ee3df2a6e9d08ee298
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2024-02-29 15:40:50 -0500

    add more configs

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
@@ -58,6 +59,9 @@ public class IntakeSubsystem extends SubsystemBase {
     // intakePivotConfig.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
     // intakePivotConfig.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
     // otbPivotMotor.getConfigurator().apply(intakePivotConfig, HardwareConstants.TIMEOUT_S);
+    // intakeConfig.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
+    // intakeConfig.Feedback.FeedbackRotorOffset = rotor offset;
+    // intakeConfig.Feedback.FeedbackRemoteSensorID = idk;
 
     TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
     intakeConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
