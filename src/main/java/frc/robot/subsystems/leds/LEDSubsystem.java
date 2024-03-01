e43a2c05491d5eee274467bfc1cf69ccc447c10c
Ishan <ishanacharyagangopadhyay@gmail.com>
2024-03-01 13:48:24 -0500

    led changes+merge conflicts

--- src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/leds/LEDSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems.leds;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.LEDConstants;
@@ -11,35 +13,59 @@ import frc.robot.Constants.LEDConstants.LEDProcess;
 
 
 public class LEDSubsystem extends SubsystemBase {
+
   private Spark ledSpark;
 
   private LEDProcess process;
 
-  /** Creates a new LEDSubsystemImpl for use with LED strips made by Spark lighting (Loopy's LEDs).
-   * @param port The Spark port for this LEDSubsystem.
-  */
-  public LEDSubsystem(int port) {
-    ledSpark = new Spark(port);
-    setProcess(LEDProcess.OFF);
-  }
+  private boolean isRed;
 
   /** Creates a new LEDSubsystemImpl with the port in LEDConstants. */
   public LEDSubsystem() {
-    ledSpark = new Spark(LEDConstants.LEDPort);
+    ledSpark = new Spark(LEDConstants.LED_PORT);
     setProcess(LEDProcess.OFF);
+    isRed = DriverStation.getAlliance().isPresent() && (DriverStation.getAlliance().get() == Alliance.Red);
   }
-  
-  @Override
-  public void periodic() {
-    
-  }
-  
   public void setProcess(LEDProcess process) {
     this.process = process;
+  }
+
+  private double getSparkFromProcess(LEDProcess pr) {
+    switch (pr) {
+      case DEFAULT:
+        return defaultColor();
+      case ALLIANCE_COLOR:
+        return allianceColor();
+      default:
+        return pr.sparkValue;
+    }
+  }
+  private double allianceColor() {
+    if (isRed) {
+      return LEDProcess.RED_ALLIANCE.sparkValue;
+    } else {
+      return LEDProcess.BLUE_ALLIANCE.sparkValue;
+    }
+  }
+
+  private double defaultColor() {
+    if (DriverStation.isAutonomous()) {
+      return LEDProcess.AUTONOMOUS.sparkValue;
+    } else {
+      return allianceColor();
+    }
   }
 
   public void off() {
     process = LEDProcess.OFF;
   }
 
+  @Override
+  public void periodic() {
+    if (process == LEDProcess.ALLIANCE_COLOR) {
+      ledSpark.set(allianceColor());
+    } else {
+      ledSpark.set(getSparkFromProcess(process));
+    }
+  }
 }
