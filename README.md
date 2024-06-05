90548a8247a228a87499f76a544dbfce7163b5be
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2024-06-05 17:48:48 -0400

    decent changes idk

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![CI](https://github.com/TitaniumTigers4829/robot-code-2024/actions/workflows/main.yml/badge.svg)](https://github.com/TitaniumTigers4829/robot-code-2024/actions/workflows/main.yml)
 
-Robot code for FRC team 4829 The Titanium Tigers 2024 robot Aquila. 
+Robot code for FRC team 4829 The Titanium Tigers 2024 robot Aquila.
 \
 \
 Includes code for:
--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -14,9 +14,12 @@ public class VisionSubsystem extends SubsystemBase {
   private double headingRateDegreesPerSecond = 0;
 
   /**
-   * The pose estimates from the limelights in the following order {shooterLimelight, frontLeftLimelight, frontRightLimelight}
+   * The pose estimates from the limelights in the following order {shooterLimelight,
+   * frontLeftLimelight, frontRightLimelight}
    */
-  private PoseEstimate[] limelightEstimates = {new PoseEstimate(), new PoseEstimate(), new PoseEstimate()};
+  private PoseEstimate[] limelightEstimates = {
+    new PoseEstimate(), new PoseEstimate(), new PoseEstimate()
+  };
 
   public VisionSubsystem() {
     visionThread(VisionConstants.SHOOTER_LIMELIGHT_NUMBER);
@@ -26,6 +29,7 @@ public class VisionSubsystem extends SubsystemBase {
 
   /**
    * Checks if the specified limelight can fully see one or more April Tag.
+   *
    * @param limelightNumber the number of the limelight
    * @return true if the limelight can fully see one or more April Tag
    */
@@ -34,61 +38,64 @@ public class VisionSubsystem extends SubsystemBase {
     // Different Limelights have different FOVs
     if (getLimelightName(limelightNumber).equals(VisionConstants.SHOOTER_LIMELIGHT_NAME)) {
       return getNumberOfAprilTags(limelightNumber) != 0
-        && Math.abs(LimelightHelpers.getTX(getLimelightName(limelightNumber))) <= VisionConstants.LL3G_FOV_MARGIN_OF_ERROR;
+          && Math.abs(LimelightHelpers.getTX(getLimelightName(limelightNumber)))
+              <= VisionConstants.LL3G_FOV_MARGIN_OF_ERROR;
     }
     return getNumberOfAprilTags(limelightNumber) != 0
-      && Math.abs(LimelightHelpers.getTX(getLimelightName(limelightNumber))) <= VisionConstants.LL3_FOV_MARGIN_OF_ERROR;
+        && Math.abs(LimelightHelpers.getTX(getLimelightName(limelightNumber)))
+            <= VisionConstants.LL3_FOV_MARGIN_OF_ERROR;
   }
 
   /**
-   * Gets the JSON dump from the specified limelight and puts it into a PoseEstimate object,
-   * which is then placed into its corresponding spot in the limelightEstimates array.
+   * Gets the JSON dump from the specified limelight and puts it into a PoseEstimate object, which
+   * is then placed into its corresponding spot in the limelightEstimates array.
+   *
    * @param limelightNumber the number of the limelight
    */
   public void updateLimelightPoseEstimate(int limelightNumber) {
-    if (canSeeAprilTags(limelightNumber)) {
-      // MegaTag2 is much more accurate, but only use it when the robot isn't rotating too fast
-      if (headingRateDegreesPerSecond < VisionConstants.MEGA_TAG_2_MAX_HEADING_RATE) {
-        LimelightHelpers.SetRobotOrientation(VisionConstants.SHOOTER_LIMELIGHT_NAME, headingDegrees, 0, 0, 0, 0, 0);
-        limelightEstimates[limelightNumber] = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(getLimelightName(limelightNumber));
-      }
-      limelightEstimates[limelightNumber] = LimelightHelpers.getBotPoseEstimate_wpiBlue(getLimelightName(limelightNumber));
-    } else {
+    if (!canSeeAprilTags(limelightNumber)) {
       limelightEstimates[limelightNumber] = new PoseEstimate();
+    }
+    // MegaTag2 is much more accurate, but only use it when the robot isn't rotating too fast
+    if (headingRateDegreesPerSecond < VisionConstants.MEGA_TAG_2_MAX_HEADING_RATE) {
+      LimelightHelpers.SetRobotOrientation(
+          VisionConstants.SHOOTER_LIMELIGHT_NAME, headingDegrees, 0, 0, 0, 0, 0);
+      limelightEstimates[limelightNumber] =
+          LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(getLimelightName(limelightNumber));
+    } else {
+      limelightEstimates[limelightNumber] =
+          LimelightHelpers.getBotPoseEstimate_wpiBlue(getLimelightName(limelightNumber));
     }
   }
 
   /**
-   * Gets the pose of the robot calculated by specified limelight via any April
-   * Tags it sees
+   * Gets the pose of the robot calculated by specified limelight via any April Tags it sees
+   *
    * @param limelightNumber the number of the limelight
-   * @return the pose of the robot, if the limelight can't see any April Tags,
-   * it will return 0 for x, y, and theta
+   * @return the pose of the robot, if the limelight can't see any April Tags, it will return 0 for
+   *     x, y, and theta
    */
   public Pose2d getPoseFromAprilTags(int limelightNumber) {
     return limelightEstimates[limelightNumber].pose;
   }
 
-  /**
-   * Returns how many april tags the limelight that is being used for pose
-   * estimation can see.
-   */
+  /** Returns how many april tags the limelight that is being used for pose estimation can see. */
   public int getNumberOfAprilTags(int limelightNumber) {
     return limelightEstimates[limelightNumber].tagCount;
   }
 
   /**
-   * Returns the timestamp for the vision measurement from the limelight that
-   * is being used for pose estimation.
+   * Returns the timestamp for the vision measurement from the limelight that is being used for pose
+   * estimation.
    */
   public double getTimeStampSeconds(int limelightNumber) {
     return limelightEstimates[limelightNumber].timestampSeconds / 1000.0;
   }
 
   /**
-   * Returns the latency in seconds of when the limelight that is being
-   * used for pose estimation calculated the robot's pose. It adds the
-   * pipeline latency, capture latency, and json parsing latency.
+   * Returns the latency in seconds of when the limelight that is being used for pose estimation
+   * calculated the robot's pose. It adds the pipeline latency, capture latency, and json parsing
+   * latency.
    */
   public double getLatencySeconds(int limelightNumber) {
     // TODO: Verify this works
@@ -97,6 +104,7 @@ public class VisionSubsystem extends SubsystemBase {
 
   /**
    * Gets the average distance between the specified limelight and the April Tags it sees
+   *
    * @param limelightNumber the number of the limelight
    * @return the average distance between the robot and the April Tag(s) in meters
    */
@@ -110,9 +118,11 @@ public class VisionSubsystem extends SubsystemBase {
   }
 
   /**
-   * Sets the heading and heading rate of the robot, this is used
-   * for deciding between MegaTag 1 and 2 for pose estimation.
-   * @param headingDegrees the angle the robot is facing in degrees (0 degrees facing the red alliance)
+   * Sets the heading and heading rate of the robot, this is used for deciding between MegaTag 1 and
+   * 2 for pose estimation.
+   *
+   * @param headingDegrees the angle the robot is facing in degrees (0 degrees facing the red
+   *     alliance)
    * @param headingRateDegrees the rate the robot is rotating, CCW positive
    */
   public void setHeadingInfo(double headingDegrees, double headingRateDegrees) {
@@ -122,6 +132,7 @@ public class VisionSubsystem extends SubsystemBase {
 
   /**
    * Gets the limelight name associated with the specified limelight number/index
+   *
    * @param limelightNumber the limelight number
    * @return 0 = limelight-shooter, 1 = limelight-left, 2 = limelight-right
    */
@@ -133,56 +144,77 @@ public class VisionSubsystem extends SubsystemBase {
     } else if (limelightNumber == 2) {
       return VisionConstants.FRONT_RIGHT_LIMELIGHT_NAME;
     }
-     throw new IllegalArgumentException("You entered a number for a non-existent limelight");
+    throw new IllegalArgumentException("You entered a number for a non-existent limelight");
   }
 
-  /**
-   * Gets the pose calculated the last time a limelight saw an April Tag
-   */
+  /** Gets the pose calculated the last time a limelight saw an April Tag */
   public Pose2d getLastSeenPose() {
     return lastSeenPose;
   }
 
   /**
-   * This makes a thread which updates the pose estimate for the specified limelight.
-   * Because we have 3 limelights, if we are trying to parse the JSON dump  for each limelight
-   * one after another every scheduler loop, we will have loop overruns, so we opted to have them 
-   * run in parallel like this.
+   * This makes a thread which updates the pose estimate for the specified limelight. Because we
+   * have 3 limelights, if we are trying to parse the JSON dump for each limelight one after another
+   * every scheduler loop, we will have loop overruns, so we opted to have them run in parallel like
+   * this.
+   *
    * @param limelightNumber the limelight number
    */
   public void visionThread(int limelightNumber) {
     try {
-      new Thread(() -> {
-        double last_TX = 0;
-        double last_TY = 0;
+      new Thread(
+              () -> {
+                double last_TX = 0;
+                double last_TY = 0;
 
-        while (true) {
-          double current_TX = LimelightHelpers.getTX(getLimelightName(limelightNumber));
-          double current_TY = LimelightHelpers.getTY(getLimelightName(limelightNumber));
+                while (true) {
+                  double current_TX = LimelightHelpers.getTX(getLimelightName(limelightNumber));
+                  double current_TY = LimelightHelpers.getTY(getLimelightName(limelightNumber));
 
-          // This checks if the limelight reading is new. The reasoning being that if the TX and TY
-          // are EXACTLY the same, it hasn't updated yet with a new reading. We are doing it this way,
-          // because to get the timestamp of the reading, you need to parse the JSON dump which can be
-          // very demanding whereas this only has to get the Network Table entries for TX and TY.
-          if (last_TX != current_TX || last_TY != current_TY) {
-            updateLimelightPoseEstimate(limelightNumber);
-            // This is to keep track of the last valid pose calculated by the limelights
-            // it is used when the driver resets the robot odometry to the limelight calculated position
-            if (canSeeAprilTags(limelightNumber)) {
-              lastSeenPose = getPoseFromAprilTags(limelightNumber);
-            }
-          }
+                  // This checks if the limelight reading is new. The reasoning being that if the TX
+                  // and TY
+                  // are EXACTLY the same, it hasn't updated yet with a new reading. We are doing it
+                  // this way,
+                  // because to get the timestamp of the reading, you need to parse the JSON dump
+                  // which can be
+                  // very demanding whereas this only has to get the Network Table entries for TX
+                  // and TY.
+                  if (last_TX != current_TX || last_TY != current_TY) {
+                    updateLimelightPoseEstimate(limelightNumber);
+                    // This is to keep track of the last valid pose calculated by the limelights
+                    // it is used when the driver resets the robot odometry to the limelight
+                    // calculated position
+                    if (canSeeAprilTags(limelightNumber)) {
+                      lastSeenPose = getPoseFromAprilTags(limelightNumber);
+                    }
+                  }
 
-          last_TX = current_TX;
-          last_TY = current_TY;
-        }
-      }).start();
+                  last_TX = current_TX;
+                  last_TY = current_TY;
+                  try {
+                    Thread.sleep(1000);
+                  } catch (Exception e) {
+                    e.printStackTrace();
+                  }
+                }
+              })
+          .start();
     } catch (Exception e) {
       e.printStackTrace();
     }
-  } 
+  }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    SmartDashboard.putString(
+        "front left Odometry",
+        getPoseFromAprilTags(VisionConstants.FRONT_LEFT_LIMELIGHT_NUMBER).toString());
 
+    SmartDashboard.putString(
+        "front right Odometry",
+        getPoseFromAprilTags(VisionConstants.FRONT_RIGHT_LIMELIGHT_NUMBER).toString());
+    SmartDashboard.putString(
+        "shooter Odometry",
+        getPoseFromAprilTags(VisionConstants.SHOOTER_LIMELIGHT_NUMBER).toString());
+  }
 }
