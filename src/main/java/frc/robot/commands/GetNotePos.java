f6a7e2cb02b48fd4c11af89c9344b4fef56c3853
Jack <85963782+JacksonElia@users.noreply.github.com>
2024-09-02 17:26:52 -0400

    Note detection WWWWWWWWWWWWWWW (#22)
    * things
    
    * NOTE DETECTION FINALLY WORKS!!!!
    
    * decent changes
    
    * whoops
    
    * bruh
    
    * apply spotless
    
    ---------
    
    Co-authored-by: Luke <ni.luke2011@gmail.com>
    Co-authored-by: Ishan1522 <iacharyagangopadhyay@students.chccs.k12.nc.us>

---
+++ src/main/java/frc/robot/commands/GetNotePos.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.extras.NoteDetector;
+import java.util.Arrays;
+
+// Gets ROBOT RELATIVE POSE (NOT FIELD RELATIVE POSE) of Note
+public class GetNotePos extends Command {
+
+  public GetNotePos() {}
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    Pose2d testPose = NoteDetector.findNotePose();
+    SmartDashboard.putString("note pos", testPose.toString());
+    double[] testArray = NoteDetector.getWorldPts();
+    SmartDashboard.putString("x and y", Arrays.toString(testArray));
+
+    Pose2d notePose = NoteDetector.findNotePose();
+    Translation2d cameraOffset = NoteDetector.applyCameraOffset(notePose);
+    SmartDashboard.putString("note pos", cameraOffset.toString());
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
